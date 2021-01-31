a6802896619eb2ec2d3110e2906d9763d919a3ac
adi161201 <deshpande.aditya16@gmail.com>
2021-01-31 22:53:08 +0530

    first commit

---
+++ .history/main_20210131122222.py
@@ -0,0 +1,14 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+print(data)
---
+++ .history/main_20210131141110.py
@@ -0,0 +1,59 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+# print(data["intents"])
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
+
+    if intent["tag"] not in labels:
+        labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(classes))]
+
+for x,doc in docs_x enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w  in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = np.array(output)
